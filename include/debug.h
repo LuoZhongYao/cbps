9544724cfdde9a47b9c02852ae9148e4a68363ac
LuoZhongYao <LuoZhongYao@gmail.com>
2016-11-23 17:50:44 +0800

    log 调整

--- include/debug.h
+++ include/debug.h
@@ -17,13 +17,26 @@ DESCRIPTION
 
 #ifdef DEBUG_PRINT_ENABLED
 
-#   define DEBUG(tag,fmt, ...)  printf("D/%-16.16s : " fmt, tag, ##__VA_ARGS__)
-#   define LOGD(fmt,...)        DEBUG(LOG_TAG, fmt, ##__VA_ARGS__)
+#   define PUTS(level, tag, fmt, ...)      printf(level "/%-16.16s : " fmt, tag, ##__VA_ARGS__)
+
 #else
-#   define DEBUG(x) 
+
+#   define PUTS(...)
+
 #endif /*DEBUG_PRINT_ENABLED*/
     /* If you want to carry out cVc license key checking in Production test
    Then this needs to be enabled */
+
+#define DEBUG(tag,fmt, ...)      PUTS("D", tag, fmt, ##__VA_ARGS__)
+#define INFO(tag,fmt, ...)       PUTS("I", tag, fmt, ##__VA_ARGS__)
+#define WARNING(tag,fmt, ...)    PUTS("W", tag, fmt, ##__VA_ARGS__)
+#define ERROR(tag, fmt, ...)     PUTS("E", tag, fmt, ##__VA_ARGS__
+
+#define LOGD(fmt,...)        DEBUG(LOG_TAG, fmt, ##__VA_ARGS__)
+#define LOGI(fmt,...)        INFO(LOG_TAG, fmt, ##__VA_ARGS__)
+#define LOGW(fmt,...)        WARNING(LOG_TAG, fmt, ##__VA_ARGS__)
+#define LOGE(fmt,...)        ERROR(LOG_TAG, fmt, ##__VA_ARGS__)
+#define NOT_IMPL()           LOGW("Not Impl %s:%d\n", __func__,__LINE__)
 
 #ifdef DEBUG_PEER
 #   define PEER_DEBUG(x) DEBUG(x)
@@ -31,6 +44,5 @@ DESCRIPTION
 #   define PEER_DEBUG(x) 
 #endif
 
-#   define NOT_IMPL()   LOGD("Not Impl %s:%d\n", __func__,__LINE__)
 
 #endif /*_SINK_DEBUG_H_*/
